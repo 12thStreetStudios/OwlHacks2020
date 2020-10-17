b944c47843abef8be90024f67ec1bd07191db0bc
Russell <rdaswimmer@gmail.com>
2020-10-17 15:16:29 -0400

    removed post from main

--- owlhacks2020/src/App.js
+++ owlhacks2020/src/App.js
@@ -42,19 +42,4 @@ function Home() {
   }
 }
 
-class Post extends React.Component {
-
-  render() {
-    return (
-      <div className="post">
-        <h3>{this.props.title}</h3>
-        {/* TODO: Add organization href link */}
-        <p id="organization"><a>{this.props.organization}</a></p>
-        {/* TODO: Add user href link*/}
-        <p id="poster">Posted by <a>{this.props.poster}</a></p>
-      </div>
-    );
-  }
-}
-
 export default App;
