22a8641b92873ffb26f77f4aacf3eca94dfa9560
Russell <rdaswimmer@gmail.com>
2020-10-17 12:52:17 -0400

    added project basics

--- owlhacks2020/src/App.js
+++ owlhacks2020/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Title from './components/Title'
 import Post from './components/Post'
+import Project from './components/Project';
 
 function App() {
   return(
@@ -12,6 +13,7 @@ function App() {
         <Post title="First Post" poster="Jimmy" organization="12th Street"/>
         <Post title="Need help with ReactJS" poster="Russell" organization="Bad Company"/>
         <Post title="Want to work on C++? We are game devs!" poster="Jimmy" organization="Philadelphia Phillies"/>
+        <Project pname="OwlHacks2020" members="Sean, Jimmy, Russell" creation_date="10/17/20" todo_list="stuff"></Project> 
       </header>
     </div>
   );
