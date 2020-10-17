e3dd49380ae6785a72f7110c370b64b90ba19c1f
invention64 <jimmygottshall@gmail.com>
2020-10-17 11:49:22 -0400

    Added a basic post class, and wrote css

--- owlhacks2020/src/App.js
+++ owlhacks2020/src/App.js
@@ -6,21 +6,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Post title="First Post" poster="Jimmy" organization="12th Street"/>
+        <Post title="Need help with ReactJS" poster="Russell" organization="Bad Company"/>
+        <Post title="Want to work on C++? We are game devs!" poster="Jimmy" organization="Philadelphia Phillies"/>
       </header>
     </div>
   );
+}
+
+class Post extends React.Component {
+
+  render() {
+    return (
+      <div className="post">
+        <h3>{this.props.title}</h3>
+        {/* TODO: Add organization href link */}
+        <p id="organization"><a>{this.props.organization}</a></p>
+        {/* TODO: Add user href link*/}
+        <p id="poster">Posted by <a>{this.props.poster}</a></p>
+      </div>
+    );
+  }
 }
 
 export default App;
