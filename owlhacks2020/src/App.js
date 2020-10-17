53ad1badaec05582e907ceefa74814c133833ad0
Russell <rdaswimmer@gmail.com>
2020-10-17 12:34:12 -0400

    moved files around.

--- owlhacks2020/src/App.js
+++ owlhacks2020/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Title from './Title'
+
+import Title from './components/Title'
 import Post from './components/Post'
 
 function App() {
