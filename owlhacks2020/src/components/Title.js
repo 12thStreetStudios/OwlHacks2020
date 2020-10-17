26dbc826aa21ef3b766edf58c8f32b0e049809cc
Russell <rdaswimmer@gmail.com>
2020-10-17 14:07:52 -0400

    changed prokect and title css

--- owlhacks2020/src/components/Title.js
+++ owlhacks2020/src/components/Title.js
@@ -6,7 +6,14 @@ function Title() {
     return (
         <div className="Title">
             <header>
-                <h1> Open Source Git Job Page </h1>
+                <h1>Dev-Helpr</h1>
+                <p>"A place to find help with open source projects"</p>
+                <div id="navbar">
+                    <ul>
+                        <li><a href ="">Login</a></li>
+                        <li><a href = "">Register</a></li>
+                    </ul>
+                </div>
             </header>
         </div>
     )
