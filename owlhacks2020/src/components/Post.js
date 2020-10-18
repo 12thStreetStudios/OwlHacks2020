2e86c4cf4dabe35682993b1f50f3561d3957ec2d
Russell <rdaswimmer@gmail.com>
2020-10-18 02:45:18 -0400

    added comment buttons and box and count

--- owlhacks2020/src/components/Post.js
+++ owlhacks2020/src/components/Post.js
@@ -24,17 +24,32 @@ class Post extends React.Component {
     render() {
         return (
           <div>
-          <div className="post" onClick={this.onClick}>
+            <div className="post" onClick={this.onClick}>
             <h3>{this.props.title}</h3>
-            {/* TODO: Add organization href link */}
-            <p className="organization"><a>{this.props.organization}</a></p>
-            {/* TODO: Add user href link*/}
-            <p className="poster">Posted by <a>{this.props.poster}</a></p>
+              <body>
+              {/* TODO: Add organization href link */}
+              <p className="organization"><a>{this.props.organization}</a></p>
+              {/* TODO: Add user href link*/}
+              <p className="poster">Posted by: <a>{this.props.poster}</a></p>
+              <p className="commentCount">Comments: {this.state.comments.length}</p>
+            </body>
           </div>
-          <ul className={this.state.expanded}>
-            {this.state.comments}
-          </ul>
-        </div>
+            <ul className={this.state.expanded}>
+              {this.state.comments}
+            </ul>
+            <div className="postbutton" onClick={this.onClick}>
+            
+            </div>
+            <div>
+              <body className={this.state.expanded}>
+                <form action="">
+                  <p>Enter Comment Below: </p>
+                  <textarea name="comment" cols="20" rows="4"></textarea>
+                  <input type="submit" name="Comment"/>
+                </form>
+              </body>
+            </div>
+          </div>
         );
     }
 }
